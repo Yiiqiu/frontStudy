07bc0f6a4262a297c49e8d8cde17b73fe696bbc9
yiqiu <yiqiu123456>
2021-02-14 14:55:39 +0800

    用axios请求猫眼数据并且渲染在页面上

--- src/views/Film/Nowplaying.vue
+++ src/views/Film/Nowplaying.vue
@@ -2,18 +2,34 @@
   <div>
     Nowplaying
     <ul>
-      <li v-for="data in datalist" :key="data" @click="handleChangePage(data)">
-         {{data}}
+      <li v-for="data in datalist" :key="data.id" @click="handleChangePage(data)">
+         {{data.nm}}
       </li>
     </ul>
   </div>
 </template>
 <script>
+import axios from 'axios'
+
 export default {
   data () {
     return {
       datalist: ['1', '2', '3']
     }
+  },
+  mounted () {
+    axios({
+      url: '/ajax/movieOnInfoList?token=&optimus_uuid=95D5BC406E0011EB8E2F1DBC23FF48409BF5E314A9BE4469A114A553C6923067&optimus_risk_level=71&optimus_code=10'
+    }).then(res => {
+      console.log(res.data)
+      this.datalist = res.data.movieList
+    }
+    )
+    // axios.get('/ajax/movieOnInfoList?token=&optimus_uuid=95D5BC406E0011EB8E2F1DBC23FF48409BF5E314A9BE4469A114A553C6923067&optimus_risk_level=71&optimus_code=10')
+    //   .then(res => {
+    //     // console.log(res.data)
+    //     this.datalist = res.data.movieList
+    //   })
   },
   methods: {
     handleChangePage (id) {
